12be9d7ccfd8d0fe340392b3c5a4429cdc5f41d0
Charu Wadhwa1 <charu.wadhwa1@accolite.com>
2023-04-17 11:38:46 +0530

    Socket code updated

--- src/app/ProjectComponents/UserComponents/notifications/notifications.component.ts
+++ src/app/ProjectComponents/UserComponents/notifications/notifications.component.ts
@@ -4,59 +4,69 @@ import { Router } from '@angular/router';
 import { NotificationService } from '../../../notification.service';
 import { Alertmessage } from 'src/app/alertmessage';
 import { MatTableDataSource } from '@angular/material/table';
-import {ServicealertService} from '../../../servicealert.service';
+import { ServicealertService } from '../../../servicealert.service';
 @Component({
   selector: 'app-notifications',
   templateUrl: './notifications.component.html',
   styleUrls: ['./notifications.component.css']
 })
-export class NotificationsComponent implements OnInit,AfterViewInit{
-  allMessages:Alertmessage[]=[];
-  messages:Alertmessage={
-      messageId: 0,
-      aircraftRegistration: "",
-      flight: "",
-      desk: "",
-      deskCategory: "",
-      escalated: "",
-      acknowledge: "",
-      acknowledgedBy: "",
-      received: "",
-      priority: "",
-      isPublished: 0
-    }
+export class NotificationsComponent implements OnInit, AfterViewInit {
+  allMessages: Alertmessage[] = [];
+  messages: Alertmessage = {
+    messageId: 0,
+    aircraftRegistration: "",
+    flight: "",
+    desk: "",
+    deskCategory: "",
+    escalated: "",
+    acknowledge: "",
+    acknowledgedBy: "",
+    received: "",
+    priority: "",
+    isPublished: 0
+  }
   clickedRows = new Set<Alertmessage>();
   displayedColumns: string[] = [
-  "aircraftRegistration",
-  "flight",
-  "desk",
-  "deskCategory",
-  "escalated",
-  "acknowledge",
-  "acknowledgedBy",
-  "received",
-  "priority",
+    "aircraftRegistration",
+    "flight",
+    "desk",
+    "deskCategory",
+    "escalated",
+    "acknowledge",
+    "acknowledgedBy",
+    "received",
+    "priority",
   ];
-  dataSource=new MatTableDataSource<Alertmessage>();
-  constructor(private route:Router, public notification: NotificationService,private message:ServicealertService)
-  {
+  dataSource = new MatTableDataSource<Alertmessage>();
+  constructor(private route: Router, public notification: NotificationService, private message: ServicealertService) {
   }
   ngAfterViewInit(): void {
-  
-  }
 
-  ngOnInit(): void{
-    if(sessionStorage.getItem('role')!=='USER' || sessionStorage.getItem('role')===null){
+  }
+  publishedMessages!: Alertmessage[];
+  ngOnInit(): void {
+    if (sessionStorage.getItem('role') !== 'USER' || sessionStorage.getItem('role') === null) {
       this.route.navigate([""]);
     }
-    this.sendMessage();
+    //TODO make an api call to get messages for user from db(published messages)
+    this.message.getPublishedData().subscribe((res) => {
+      this.publishedMessages = res;
+      this.dataSource.data = this.publishedMessages;
+    })
+    this.notification.messageSubject.asObservable().subscribe((res) => {
+      this.publishedMessages = [...this.publishedMessages, res as Alertmessage];
+      this.dataSource.data = this.publishedMessages;
+    })
   }
-  logOut(){
+  logOut() {
     alert("Logging off");
     sessionStorage.clear();
     this.route.navigate([""]);
   }
   sendMessage() {
-    this.dataSource.data=this.notification.msg;
-   }
+    // this.dataSource.data=this.notification.getMessages() as Alertmessage;
+    //console.log(this.notification)
+
+    // console.log(this.notification.getMessages() as Alertmessage[]);
+  }
 }
